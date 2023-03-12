de150d51656139185700e4508732f470703c66d6
Chris-code240 <duah.marfochristian@gmail.com>
2023-03-12 01:46:03 +0000

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+      colors:{
+        darkblue:"#000c18",
+        darkblueish:"#547cc2",
+        blueish:"#236dc2",
+        pinkish:"#8a5380",
+        yellowish:"#beaa34",
+        creamish:"#a6987e"
+      }
+    },
+  },
+  plugins: [],
+}
